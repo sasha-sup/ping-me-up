b31fccffa2d26cc625ce8aebc59958b5d7794777
sasha-sup <a.suprun1312@gmail.com>
2023-10-27 10:31:09 +0400

    feat: add top cpu\mem\dick processes

--- monitor-me.sh
+++ monitor-me.sh
@@ -20,6 +20,12 @@ disk_usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 # Get public IP address
 public_ip="🌐 IP: $(curl -4 ifconfig.me)"
 
+# Shit happens
+top_cpu_process=$(ps aux --sort=-%cpu | awk 'NR==2')
+top_memory_process=$(ps aux --sort=-%mem | awk 'NR==2')
+largest_file=$(find / -type f -exec du -h {} + 2>/dev/null | sort -rh | head -n 1)
+
+
 send_telegram_message() {
     local message="$1"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
@@ -30,21 +36,25 @@ send_telegram_message() {
 message=""
 if (( $(echo "$cpu_usage > $cpu_threshold" | bc -l) )); then
     message+="
+$public_ip
 🚨 CPU usage: $cpu_usage%
-$public_ip"
+Top CPU-consuming process: $top_cpu_process
+"
 fi
 
 if (( $(echo "$ram_usage > $ram_threshold" | bc -l) )); then
     message+="
-🚨 RAM usage: $ram_usage%
 $public_ip
+🚨 RAM usage: $ram_usage%
+Top memory-consuming process: $top_memory_process
 "
 fi
 
 if (( $(echo "$disk_usage > $disk_threshold" | bc -l) )); then
     message+="
-🚨 Disk space below $disk_usage%
 $public_ip
+🚨 Disk space below $disk_usage%
+Largest file on disk: $largest_file
 "
 fi
 
