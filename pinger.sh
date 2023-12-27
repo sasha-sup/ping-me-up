d52f1e97b3c899c6309296c725331a29852da5bd
t.me/sasha_sup <a.suprun1312@gmail.com>
2023-12-27 13:11:55 +0400

    feat: netcat

--- pinger.sh
+++ pinger.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-if [[ -f .env ]]; then
-    source .env
+ENV_FILE="/home/sasha/Code/ping-me-up/.env"
+PORT=22
+
+if [[ -f "$ENV_FILE" ]]; then
+    source "$ENV_FILE"
 else
-    echo "Where is .env?"
+    echo "Where is $ENV_FILE?"
     exit 1
 fi
 
 for HOST in "${HOSTS[@]}"; do
-    if ! ping -c 1 $HOST &> /dev/null; then
-        MESSAGE="❌ $HOST is down"
+    IFS=" " read -r NAME IP <<< "$HOST"
+
+    if nc -zv -w 2 "$IP" "$PORT" &> /dev/null; then
+        MESSAGE="✅ $NAME ($IP) is reachable on port $PORT"
+        echo $MESSAGE
+        # curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESSAGE"
+    else
+        MESSAGE="❌ $NAME ($IP) is down on port $PORT"
         curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d "chat_id=$CHAT_ID&text=$MESSAGE"
     fi
 done
