d7b9b4e5334396e2b5bd45ddf2e3625a4fb95ceb
t.me/sasha_sup <a.suprun1312@gmail.com>
2024-03-26 21:14:31 +0400

    fix: dick trashhold, ps, msg temlpt

--- monitor-me.sh
+++ monitor-me.sh
@@ -10,7 +10,7 @@ fi
 # Percentage thresholds
 cpu_threshold=70
 ram_threshold=70
-disk_threshold=30
+disk_threshold=70
 
 # Get metrics
 cpu_usage=$(top -b -n 1 | grep "Cpu(s)" | awk '{print $2}')
@@ -18,11 +18,11 @@ ram_usage=$(free | grep Mem | awk '{print ($3 / $2) * 100}')
 disk_usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
 # Get public IP address
-public_ip="🌐 IP: $(curl -4 ifconfig.me)"
+public_ip=$(curl -4 ifconfig.me)
 
 # Shit happens
-top_cpu_process=$(ps aux --sort=-%cpu | awk 'NR==2')
-top_memory_process=$(ps aux --sort=-%mem | awk 'NR==2')
+top_cpu_process=$(ps -eo pid,cmd,comm,%cpu --sort=-%cpu | head -2)
+top_memory_process=$(ps -eo pid,cmd,comm,%mem --sort=-%mem | head -2)
 largest_file=$(find / -type f -exec du -h {} + 2>/dev/null | sort -rh | head -n 1)
 
 
@@ -36,25 +36,28 @@ send_telegram_message() {
 message=""
 if (( $(echo "$cpu_usage > $cpu_threshold" | bc -l) )); then
     message+="
-$public_ip
-🚨 CPU usage: $cpu_usage%
-Top CPU-consuming process: $top_cpu_process
+🚨  🚨  🚨 
+🌐 IP: $public_ip
+⚙️ CPU usage: $cpu_usage%
+$top_cpu_process
 "
 fi
 
 if (( $(echo "$ram_usage > $ram_threshold" | bc -l) )); then
     message+="
-$public_ip
-🚨 RAM usage: $ram_usage%
-Top memory-consuming process: $top_memory_process
+🚨  🚨  🚨 
+🌐 IP: $public_ip
+⚙️ RAM: $ram_usage%
+$top_memory_process
 "
 fi
 
 if (( $(echo "$disk_usage > $disk_threshold" | bc -l) )); then
     message+="
-$public_ip
-🚨 Disk space below $disk_usage%
-Largest file on disk: $largest_file
+🚨  🚨  🚨 
+🌐 IP: $public_ip
+⚙️ Disk space below: $disk_usage%
+$largest_file
 "
 fi
 
